1df6e1cd7da163123c11ddca2e00b34777664283
stanislav <xiiiovart@gmail.com>
2022-12-20 14:09:01 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from "./App.module.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      Hello App
+    </div>
+  );
+}
+
+export default App;
